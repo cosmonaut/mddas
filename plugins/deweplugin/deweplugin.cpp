e1b5edef84a7b8e27812eaeec002c9e8973ae961
cosmonaut <nicholas.nell@colorado.edu>
2013-10-29 11:33:16 -0600

    Very basic deweplugin packet logic added

--- plugins/deweplugin/deweplugin.cpp
+++ plugins/deweplugin/deweplugin.cpp
@@ -5,6 +5,7 @@
 */
 
 #include <QDebug>
+#include <stdint.h>
 
 #include "socket.h"
 #include "deweplugin.h"
@@ -18,6 +19,7 @@ DewePlugin::DewePlugin() : SamplingThreadPlugin() {
 
     try {
         _serv_sock = new TCPServerSocket(DEWESOFT_PORT, 1);
+        qDebug() << "Listening on port " << DEWESOFT_PORT;
     } catch (SocketException &e) {
         qDebug() << e.what();
         _serv_sock = NULL;
@@ -53,7 +55,22 @@ void DewePlugin::run() {
     QVector<MDDASDataPoint> v;
     sleepM.lock();
     int ret = 0;
+    /* Packet buffer */
+    uint8_t pak_buf[1400];
+    uint8_t p_status = 1;
 
+    /* packet internals */
+    int32_t psize = 0;
+    int32_t ptype = 0;
+    int32_t psamps = 0;
+    int64_t ptsamps = 0;
+    double pts = 0.0;
+    uint64_t pstart = 0;
+    uint64_t pend = 0;
+    /* pointers for casts... */
+    uint64_t* p_start_p;
+
+    qDebug() << "Waiting for client connection...";
     /* Loop waiting for a client to connect */
     forever {
         if (_serv_sock) {
@@ -61,10 +78,10 @@ void DewePlugin::run() {
             if (ret == 1) {
                 try {
                     _sock = _serv_sock->accept();
-                    qDebug() << "accept() client connection";
+                    qDebug() << "Client connected";
                     QString addrstr = QString::fromStdString(_sock->getForeignAddress());
-                    qDebug() << "Address: " << addrstr;
-                    qDebug() << "Port: " << _sock->getForeignPort();
+                    qDebug() << "Client Address: " << addrstr;
+                    qDebug() << "Client Port: " << _sock->getForeignPort();
 
                 } catch (SocketException &e) {
                     qDebug() << "FAILED TO ACCEPT?";
@@ -84,7 +101,8 @@ void DewePlugin::run() {
             return;
         }
 
-        condition.wait(&sleepM, 500);
+        /* Check 10 times per second */
+        condition.wait(&sleepM, 100);
     }
 
     /* Data loop. */
@@ -101,8 +119,84 @@ void DewePlugin::run() {
         }
 
         /* Get data from DeweTron DeweSoft. */
+        ret = _sock->select();
+        if (ret > 0) {
+            ret = _sock->recv(pak_buf, sizeof(pak_buf));
+            if (ret < 0) {
+                qDebug() << "recv error: " << ret;
+            } else if (ret == 0) {
+                /* TCP connection closed, abort so the plugin can be
+                   reloaded... */
+                qDebug() << "CONNECTION CLOSED!";
+                abort = true;
+            } else {
+                // header: 36 bytes
+                // body: depends on # of channels
+                // footer: 8 bytes
+                qDebug() << "read " << ret << " bytes";
+                
+                /* From Kyung: They are synchronous parallel data with
+                   16 channels all in 16bit counts (integer) */
+                /* We need to have some minimum size to read basica
+                   parameters... */
+                if (ret > 43) {
 
-        condition.wait(&sleepM, 1);
+                    /* point to packet start string */
+                    p_start_p = ((uint64_t *)(pak_buf + 0));
+                    /* deref packet start string... */
+                    pstart = *p_start_p;
+                    //qDebug() << pstart;
+
+                    if (pstart != (uint64_t)0x0706050403020100) {
+                        qDebug() << "Bad packet start string!";
+                        p_status = 0;
+                    }
+
+                    /* Packet size in bytes */
+                    psize = *((int32_t *)(pak_buf + 8));
+                    qDebug() << "packet size: " << psize;
+
+                    if ((psize + 16) == ret) {
+                        qDebug() << "PACKET SIZE SUCCESS";
+                    }
+
+                    /* packet type */
+                    ptype = *((int32_t *)(pak_buf + 12));
+                    qDebug() << "Packet type: " << ptype;
+
+                    /* samples per channel */
+                    psamps = *((int32_t *)(pak_buf + 16));
+                    qDebug() << "samples: " << psamps;
+
+                    /* number of samples */
+                    ptsamps = *((int64_t *)(pak_buf + 20));
+                    qDebug() << "total samps: " << ptsamps;
+
+                    /* timestamp */
+                    pts = *((double *)(pak_buf + 28));
+                    qDebug() << "timestamp: " << pts;
+                }
+
+                if (p_status) {
+                    /* deser packet and send to gui */
+                    qDebug() << "deser";
+                }
+
+                if (ret >= (psize + 16)) {
+                    /* Check end of packet? */
+                    pend = *((uint64_t *)(pak_buf + psize + 8));
+                    if (pend != (uint64_t)0x0001020304050607) {
+                        qDebug() << "Bad packet end";
+                    }
+                } else {
+                    qDebug() << "packet not large enough for end string";
+                }
+
+                p_status = 1;
+            }
+        }
+
+        condition.wait(&sleepM, 50);
     }
 }
 
--- scripts/dewesoft/dewecli.cpp
+++ scripts/dewesoft/dewecli.cpp
@@ -1,7 +1,9 @@
-/* Simple script to make a connection to the deweplugin */
+/* Simple script to make a test connection to the deweplugin */
 
 #include <iostream>           // For cerr and cout
 #include <cstdlib>            // For atoi()
+#include <unistd.h>           // sleep()...
+#include <stdint.h>
 
 #include "socket.h"  // Gotta have socket magic
 
@@ -9,13 +11,13 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        cerr << "Usage: " << argv[0] 
+        cerr << "Usage: " << argv[0]
              << " <Server> <Server Port>" << endl;
         exit(1);
     }
 
     string servAddress = argv[1]; // First arg: server address
-    unsigned short echoServPort = atoi(argv[2]);
+    unsigned short echoServPort = atoi(argv[2]); // second arg port
 
     cout << "server: " << servAddress << endl;
     cout << "port: " << echoServPort << endl;
@@ -23,8 +25,50 @@ int main(int argc, char *argv[]) {
     try {
         // Establish connection with the echo server
         TCPSocket sock(servAddress, echoServPort);
-  
-        // Destructor closes the socket
+        /* packet buffer */
+        uint8_t pak[1400];
+
+        //(uint64_t)pak[0] = 0x0001020304050607;
+        //((uint64_t *)pak)[0] = 0x0001020304050607;
+        pak[0] = 0x00;
+        pak[1] = 0x01;
+        pak[2] = 0x02;
+        pak[3] = 0x03;
+        pak[4] = 0x04;
+        pak[5] = 0x05;
+        pak[6] = 0x06;
+        pak[7] = 0x07;
+
+        /* DEWEsoft packet size in bytes */
+        *((int32_t *)(pak + 8)) = (int32_t)28;
+        /* packet type */
+        *((int32_t *)(pak + 12)) = (int32_t)0;
+        /* samples per channel */
+        *((int32_t *)(pak + 16)) = (int32_t)1;
+        /* samples acquired so far ... */
+        *((int64_t *)(pak + 20)) = (int64_t)1;
+        /* absolute/relative time (days) */
+        *((double *)(pak + 28)) = (double)3.14159265;
+
+
+        *((uint64_t *)(pak + 36)) = (uint64_t)0x0001020304050607;
+
+        sock.send(pak, 44);
+
+        sleep(5);
+
+        // pak[0] = 0x07;
+        // pak[1] = 0x06;
+        // pak[2] = 0x05;
+        // pak[3] = 0x04;
+        // pak[4] = 0x03;
+        // pak[5] = 0x02;
+        // pak[6] = 0x01;
+        // pak[7] = 0x00;
+
+
+        sleep(5);
+        /* Destructor closes the socket */
 
     } catch(SocketException &e) {
         cerr << e.what() << endl;
