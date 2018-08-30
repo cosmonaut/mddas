4deecec2dccf7c5cac8a794dd3ab3a45b4c4cbcf
cosmonaut <nicholas.nell@colorado.edu>
2018-08-30 10:49:57 -0600

    Have window be max size by default

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,10 +1,23 @@
 #include <QApplication>
+#include <QFont>
 
 #include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+
+    QFont f = app.font();
+    //f.setFamily("Monaco");
+    f.setPointSize(8);
+    app.setFont(f);
+
     MainWindow window;
-    window.show();
+    // normal display
+    //window.show();
+    // nice maximal size display by default
+    window.showMaximized();
+    // hard full screen
+    //window.showFullScreen();
+
     return app.exec();
 }
